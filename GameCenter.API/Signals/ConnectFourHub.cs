15a029919f23c39f698d3d02acdbb6a75aa7db75
Arlan mendes <arlan.mendes@techinnov.ca>
2023-12-20 12:57:08 -0500

    start project

---
+++ GameCenter.API/Signals/ConnectFourHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace GameCenter.API.signals
+{
+    public class ConnectFourHub : Hub
+    {
+        public async Task Play(int column)
+        {
+            await Clients.Others.SendAsync("Opponent", column);
+        }
+
+        public async Task Winner(string user)
+        {
+            await Clients.Others.SendAsync("Winner", user);
+        }
+    }
+
+    public class PlayDto
+    {
+        public string User { get; set; }
+        public string Column { get; set; }
+    }
+}
