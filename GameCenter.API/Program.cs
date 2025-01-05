ec36fc76407321c0ee58030a232f8c106366bc09
Arlan mendes <arlanmendes197@gmail.com>
2025-01-05 12:43:16 -0500

    remove validantion

--- GameCenter.API/Program.cs
+++ GameCenter.API/Program.cs
@@ -20,7 +20,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        ValidateIssuerSigningKey = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtSecret))
                     };
 
